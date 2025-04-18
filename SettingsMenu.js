3af01343becb65836082312d576e943ef806de0d
najimrko <ali-evolution@hotmail.com>
2025-04-18 14:48:08 +0300

    Add files via upload
    Signed-off-by: najimrko <ali-evolution@hotmail.com>

---
+++ SettingsMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SettingsMenu = () => {
+  return (
+    <div>
+      <h2>Settings</h2>
+      <ul>
+        <li>Account</li>
+        <li>Notifications</li>
+        <li>Privacy</li>
+        <li>Theme</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SettingsMenu;
