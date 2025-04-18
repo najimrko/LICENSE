dd8940460d1ae0292e8b349e88a357a8ea0d5a08
najimrko <ali-evolution@hotmail.com>
2025-04-18 14:05:02 +0300

    Add files via upload
    Signed-off-by: najimrko <ali-evolution@hotmail.com>

---
+++ Signup.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    // Simulate signup process
+    console.log('User signed up:', { username, email, password });
+  };
+
+  return (
+    <form onSubmit={handleSignup}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
