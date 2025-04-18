fc3c397ab0b7e67e10baba7ac758be331c2982ee
najimrko <ali-evolution@hotmail.com>
2025-04-18 14:13:25 +0300

    Add files via upload
    Signed-off-by: najimrko <ali-evolution@hotmail.com>

---
+++ Login.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Simulate login process
+    console.log('User logged in:', { email, password });
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default Login;
